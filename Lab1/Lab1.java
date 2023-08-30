eb433fe1d0f5fc21edde975d7c31b607196f081e
pattrob <92001075+pattrob@users.noreply.github.com>
2023-08-30 13:15:39 -0400

    Add files via upload

---
+++ Lab1/Lab1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Lab1
+{
+    public static void main(String[] args)
+    {
+        int num = 121 % 60;
+        System.out.println(num);
+        
+        
+        
+        
+        
+        
+
+        
+        
+    }
+    
+}
